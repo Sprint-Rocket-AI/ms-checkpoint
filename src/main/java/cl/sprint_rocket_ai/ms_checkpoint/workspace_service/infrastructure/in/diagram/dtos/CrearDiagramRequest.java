e0f95eb1f8e6a17435d5e21f14ef00d647b80c49
matias_valdes <matias.valdes@bicevida.cl>
2026-07-05 20:39:06 -0400

    refactor(diagram):

--- src/main/java/cl/sprint_rocket_ai/ms_checkpoint/workspace_service/infrastructure/in/diagram/dtos/CrearDiagramRequest.java
+++ src/main/java/cl/sprint_rocket_ai/ms_checkpoint/workspace_service/infrastructure/in/diagram/dtos/CrearDiagramRequest.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.util.List;
 
 @Schema(description = "Solicitud para crear un nuevo diagrama")
 public record CrearDiagramRequest(
